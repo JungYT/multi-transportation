51a076814b93b240de81d2db87a14e7a975359bb
jung <skfmsgka@snu.ac.kr>
2021-04-11 00:14:51 +0900

    Give up multi processing at now..

--- test.py
+++ test.py
@@ -9,22 +9,24 @@ import fym.logging as logging
 
 
 class Test(BaseEnv):
-    def __init__(self):
+    def __init__(self, a, b):
         super().__init__()
         self.pos = BaseSystem(np.array([1]))
         self.vel = BaseSystem(np.array([0]))
+        self.a = a
+        self.b = b
 
     def set_dot(self, u):
         pos = self.pos.state
         vel = self.pos.state
         self.pos.dot = vel
-        self.vel.dot = -a * vel - b * pos
+        self.vel.dot = -self.a * vel - self.b * pos + u
 
     def reset(self):
         super().reset()
 
     def step(self):
-        *_, done = self.update()
+        *_, done = self.update(u=0.5)
         info = {
             'pos': self.pos.state,
             'vel': self.vel.state,
@@ -32,8 +34,21 @@ class Test(BaseEnv):
         return done, info
 
 
-def main():
-    env = Test()
+def main(a, b, num_parallel):
+    path = os.path.join('log', datetime.today().strftime('%Y%m%d-%H%M%S'), f"{num_parallel}")
+    train_path = os.path.join(path, 'train')
+    parameter_logger = logging.Logger(
+        log_dir=path, file_name="parameters.h5"
+    )
+    parameters  = {
+        'a': a,
+        'b': b,
+        'num_parallel': num_parallel,
+    }
+    parameter_logger.record(**parameters)
+    parameter_logger.close()
+
+    env = Test(a, b)
     env.reset()
     logger = logging.Logger(
         log_dir=train_path, file_name=f"data_{num_parallel:05d}.h5"
@@ -47,23 +62,10 @@ def main():
     logger.close()
     env.close()
 
-def run_main(parameters):
-    for k, v in parameters.items():
-        exec('{key} = {value}'.format(key=k, value=v))
-    path = os.path.join('log', datetime.today().strftime('%Y%m%d-%H%M%S'), f"{num_parallel}")
-    train_path = os.path.join(path, 'train')
-    parameter_logger = logging.Logger(
-        log_dir=path, file_name="parameters.h5"
-    )
-    parameter_logger.record(**parameters)
-    parameter_logger.close()
-
-    main()
 
 if __name__ == "__main__":
-    p1 = {'a': 1, 'b': 2, 'num_parallel':0}
-    p2 = {'a': 2, 'b': 3, 'num_parallel':1}
-    param = [p1, p2]
-    pool = Pool()
-    pool.map(run_main, param)
+    param1 = [1, 1, 0]
+    param2 = [2, 1, 1]
+    param = [param1, param2]
+    map(main, param)
 

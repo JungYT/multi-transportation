Add batch normalization and bug fix
Batch normalization is added.
Bug fix
1. Last layer of critic network had ReLU activation which has to be not
included.

dynamics.py
@@ -16,7 +16,6 @@ import fym.core as core
 from fym.utils import rot
 from utils import hat, unhat, block_diag
 
-# np.random.seed(0)
 
 class Load(BaseEnv):
     def __init__(self, mass, J):
@@ -238,17 +237,20 @@ class MultiQuadSlungLoad(BaseEnv):
         load_pos_des, load_att_des, psi_des = des
         quad_att_des, f_des = self.transform_action2des(action, psi_des)
         *_, time_out = self.update(quad_att_des=quad_att_des, f_des=f_des)
-        # *_, time_out = self.update(load_pos_des=np.vstack((0., 0., 0.)),
-        #                            load_dcm_des = np.eye(3))
         done = self.terminate(time_out)
         obs = self.observe(f_des)
-        reward, tension, tension_des = self.get_reward(load_pos_des, load_att_des, f_des)
+        reward, tension, tension_des, e = self.get_reward(
+            load_pos_des,
+            load_att_des,
+            f_des
+        )
         info = {
             'time': self.clock.get(),
             'reward': reward,
             'action': action,
             'tension': tension,
-            'tension_des': tension_des
+            'tension_des': tension_des,
+            'error': e,
         }
         return obs, reward, done, info
 
@@ -266,10 +268,8 @@ class MultiQuadSlungLoad(BaseEnv):
                 + self.load.dcm.state.dot(link.anchor) \
                 - link.len*link.uvec.state
             quad_vel[i] = self.load.vel.state \
-                + self.load.dcm.state.dot(
-                    hat(self.load.omega.state).dot(link.anchor)
-                ) \
-                - link.len*hat(link.omega.state).dot(link.uvec.state)
+                + self.load.dcm.dot.dot(link.anchor) \
+                - link.len*link.uvec.dot
             quad_att[i] = np.array(rot.dcm2angle(quad.dcm.state.T))[::-1]
             anchor_pos[i] = self.load.pos.state \
                 + self.load.dcm.state.dot(link.anchor)
@@ -372,11 +372,11 @@ class MultiQuadSlungLoad(BaseEnv):
         for i, (link, quad) in enumerate(
             zip(self.links.systems, self.quads.systems)
         ):
+            _, azi, polar = rot.cart2sph2(link.uvec.state)
             u = f_des[i] * quad.dcm.state.dot(self.e3)
-            uvec_dot = hat(link.omega.state).dot(link.uvec.state)
             obs[i] = np.hstack([
-                link.uvec.state.reshape(-1,), 
-                uvec_dot.reshape(-1,),
+                np.array([azi, polar]),
+                link.uvec.dot.reshape(-1,),
                 u.reshape(-1,)
             ])
 
@@ -387,12 +387,19 @@ class MultiQuadSlungLoad(BaseEnv):
         tension_des = self.calculate_desired_tension(F_d, M_d)
         reward = [None]*self.cfg.quad.num
         tension = [None]*self.cfg.quad.num
+        e = [None]*self.cfg.quad.num
         for i, quad in enumerate(self.quads.systems):
             u = f_des[i] * quad.dcm.state.dot(self.e3)
             tension[i] = quad.mass*self.g + u
-            e = tension[i] - np.vstack(tension_des[i])
-            reward[i] = -e.T.dot(e).reshape(-1)
-        return reward, tension, tension_des
+            e[i] = tension[i] - np.vstack(tension_des[i])
+            reward[i] = -e[i].T.dot(e[i]).reshape(-1,)
+            # mag, azi, polar = rot.cart2sph2(tension[i])
+            # mag_des, azi_des, polar_des = rot.cart2sph2(
+            #     np.array(tension_des[i])
+            # )
+            # e[i] = np.array([mag-mag_des, azi-azi_des, polar-polar_des])
+            # reward[i] = -e[i].dot(self.cfg.ddpg.P.dot(e[i].T)).reshape(-1,)
+        return reward, tension, tension_des, e
 
     def transform_action2des(self, action, psi_des):
         f_des = [None]*self.cfg.quad.num

main.py
@@ -78,18 +78,24 @@ def load_config():
     cfg.controller.Ks = 80.
     cfg.controller.chattering_bound = 0.5
     cfg.controller.unc_max = 0.1
-    cfg.controller.Kpos = 5.
-    cfg.controller.Kvel = 3.
-    cfg.controller.Kdcm = 1.
-    cfg.controller.Komega = 1.
+    cfg.controller.Kpos = 1.
+    cfg.controller.Kvel = 0.5
+    cfg.controller.Kdcm = 0.1
+    cfg.controller.Komega = 0.05
 
     cfg.ddpg = SN()
-    cfg.ddpg.P = np.diag([1., 1., 1., 5., 5., 2.])
+    cfg.ddpg.P = np.diag([1., 1., 1.])
     cfg.ddpg.reward_max = 120
-    cfg.ddpg.state_dim = 9
+    cfg.ddpg.state_dim = 8
     cfg.ddpg.action_dim = 3
-    cfg.ddpg.action_scaling = torch.Tensor([2.5, np.pi, np.pi/12])
-    cfg.ddpg.action_bias = torch.Tensor([12.5, 0, np.pi/12])
+    cfg.ddpg.action_scaling = torch.tensor(
+        [3., np.pi, np.pi/12],
+        requires_grad=True
+    )
+    cfg.ddpg.action_bias = torch.tensor(
+        [15., 0., np.pi/12],
+        requires_grad=True
+    )
     cfg.ddpg.memory_size = 20000
     cfg.ddpg.actor_lr = 0.0001
     cfg.ddpg.critic_lr = 0.001
@@ -110,24 +116,24 @@ class ActorNet(nn.Module):
         super(ActorNet, self).__init__()
         self.lin1 = nn.Linear(cfg.ddpg.state_dim, 32)
         self.lin2 = nn.Linear(32, 64)
-        self.lin3 = nn.Linear(64, 128)
-        self.lin4 = nn.Linear(128, 64)
-        self.lin5 = nn.Linear(64, 32)
-        self.lin6 = nn.Linear(32, 16)
-        self.lin7 = nn.Linear(16, cfg.ddpg.action_dim)
+        self.lin3 = nn.Linear(64, 32)
+        self.lin4 = nn.Linear(32, 16)
+        self.lin5 = nn.Linear(16, cfg.ddpg.action_dim)
         self.relu = nn.ReLU()
         self.tanh = nn.Tanh()
+        self.bn1 = nn.BatchNorm1d(32)
+        self.bn2 = nn.BatchNorm1d(64)
+        self.bn3 = nn.BatchNorm1d(32)
+        self.bn4 = nn.BatchNorm1d(16)
         self.cfg = cfg
 
     def forward(self, state):
-        x1 = self.relu(self.lin1(state))
-        x2 = self.relu(self.lin2(x1))
-        x3 = self.relu(self.lin3(x2))
-        x4 = self.relu(self.lin4(x3))
-        x5 = self.relu(self.lin5(x4))
-        x6 = self.relu(self.lin6(x5))
-        x7 = self.tanh(self.lin7(x6))
-        x_scaled = x7 * self.cfg.ddpg.action_scaling \
+        x1 = self.relu(self.bn1(self.lin1(state)))
+        x2 = self.relu(self.bn2(self.lin2(x1)))
+        x3 = self.relu(self.bn3(self.lin3(x2)))
+        x4 = self.relu(self.bn4(self.lin4(x3)))
+        x5 = self.tanh(self.lin5(x4))
+        x_scaled = x5 * self.cfg.ddpg.action_scaling \
             + self.cfg.ddpg.action_bias
         return x_scaled
 
@@ -142,15 +148,21 @@ class CriticNet(nn.Module):
         self.lin6 = nn.Linear(32, 16)
         self.lin7 = nn.Linear(16, 1)
         self.relu = nn.ReLU()
+        self.bn1 = nn.BatchNorm1d(32)
+        self.bn2 = nn.BatchNorm1d(64)
+        self.bn3 = nn.BatchNorm1d(128)
+        self.bn4 = nn.BatchNorm1d(64)
+        self.bn5 = nn.BatchNorm1d(32)
+        self.bn6 = nn.BatchNorm1d(16)
 
     def forward(self, state_w_action):
-        x1 = self.relu(self.lin1(state_w_action))
-        x2 = self.relu(self.lin2(x1))
-        x3 = self.relu(self.lin3(x2))
-        x4 = self.relu(self.lin4(x3))
-        x5 = self.relu(self.lin5(x4))
-        x6 = self.relu(self.lin6(x5))
-        x7 = self.relu(self.lin7(x6))
+        x1 = self.relu(self.bn1(self.lin1(state_w_action)))
+        x2 = self.relu(self.bn2(self.lin2(x1)))
+        x3 = self.relu(self.bn3(self.lin3(x2)))
+        x4 = self.relu(self.bn4(self.lin4(x3)))
+        x5 = self.relu(self.bn5(self.lin5(x4)))
+        x6 = self.relu(self.bn6(self.lin6(x5)))
+        x7 = self.lin7(x6)
         return x7
 
 
@@ -185,11 +197,16 @@ class DDPG:
     def get_sample(self):
         sample = random.sample(self.memory, self.cfg.ddpg.batch_size)
         state, action, reward, state_next, epi_done = zip(*sample)
-        x = torch.tensor(state, requires_grad=True).float()
-        u = torch.tensor(action, requires_grad=True).float()
-        r = torch.tensor(reward, requires_grad=True).float()
-        xn = torch.tensor(state_next, requires_grad=True).float()
-        done = torch.tensor(epi_done, requires_grad=True).float().view(-1,1)
+        # x = torch.tensor(state, requires_grad=True).float()
+        # u = torch.tensor(action, requires_grad=True).float()
+        # r = torch.tensor(reward, requires_grad=True).float()
+        # xn = torch.tensor(state_next, requires_grad=True).float()
+        # done = torch.tensor(epi_done, requires_grad=True).float().view(-1,1)
+        x = torch.FloatTensor(state)
+        u = torch.FloatTensor(action)
+        r = torch.FloatTensor(reward)
+        xn = torch.FloatTensor(state_next)
+        done = torch.FloatTensor(epi_done).view(-1,1)
         return x, u, r, xn, done
 
     def train(self):
@@ -204,12 +221,23 @@ class DDPG:
         critic_loss.backward()
         self.critic_optim.step()
 
+        # Q_w_noise_action_tmp = self.behavior_critic(torch.cat([x,u], 1))
+        # critic_loss_tmp = self.mse(Q_w_noise_action_tmp, target)
+        # if critic_loss_tmp - critic_loss == 0.:
+        #     print("Critic loss does not change:", critic_loss_tmp-critic_loss)
+
         action_wo_noise = self.behavior_actor(x)
         Q = self.behavior_critic(torch.cat([x, action_wo_noise],1))
         self.actor_optim.zero_grad()
         actor_loss = torch.sum(-Q)
         actor_loss.backward()
         self.actor_optim.step()
+
+        # action_wo_noise_tmp = self.behavior_actor(x)
+        # Q_tmp = self.behavior_critic(torch.cat([x, action_wo_noise_tmp],1))
+        # actor_loss_tmp = torch.sum(-Q_tmp)
+        # if actor_loss_tmp - actor_loss == 0.:
+        #     print("Actor loss does not change:", actor_loss_tmp-actor_loss)
 
         softupdate(
             self.target_actor,
@@ -228,8 +256,21 @@ class DDPG:
             'behavior_critic': self.behavior_critic.state_dict()
         }, path_save)
 
+    def set_train_mode(self):
+        self.behavior_actor.train()
+        self.behavior_critic.train()
+        self.target_actor.train()
+        self.target_critic.train()
+
+    def set_eval_mode(self):
+        self.behavior_actor.eval()
+        self.behavior_critic.eval()
+        self.target_actor.eval()
+        self.target_critic.eval()
+
 
 def train(agent, des, cfg, noise, env):
+    agent.set_train_mode()
     x = env.reset()
     # x = env.reset(fixed_init=True)
     noise.reset()
@@ -246,8 +287,10 @@ def train(agent, des, cfg, noise, env):
         ])
         action = np.clip(
             agent.get_action(x) + noise_set,
-            np.array(-cfg.ddpg.action_scaling + cfg.ddpg.action_bias),
-            np.array(cfg.ddpg.action_scaling + cfg.ddpg.action_bias)
+            -cfg.ddpg.action_scaling.detach().numpy() \
+            + cfg.ddpg.action_bias.detach().numpy(),
+            cfg.ddpg.action_scaling.detach().numpy() \
+            + cfg.ddpg.action_bias.detach().numpy()
         )
         xn, r, done, _ = env.step(action, des)
         for i in range(cfg.quad.num):
@@ -263,6 +306,7 @@ def train(agent, des, cfg, noise, env):
 
 
 def evaluate(env, agent, des, cfg, dir_env_data, dir_agent_data):
+    agent.set_eval_mode()
     env.logger = logging.Logger(dir_env_data)
     env.logger.set_info(cfg=cfg)
     logger_agent = logging.Logger(dir_agent_data)
@@ -286,7 +330,7 @@ def main():
     noise = OrnsteinUhlenbeckNoise(
         cfg.noise.rho,
         cfg.noise.mu,
-        np.array(cfg.ddpg.action_scaling),
+        cfg.ddpg.action_scaling.detach().numpy(),
         cfg.dt,
         cfg.ddpg.action_dim
     )
@@ -299,7 +343,7 @@ def main():
             dir_save = Path(cfg.dir, f"epi_after_{epi_num+1:05d}")
             dir_env_data = Path(dir_save, "env_data.h5")
             dir_agent_data = Path(dir_save, "agent_data.h5")
-            dir_agent_params = Path(dir_save, "agent_params.h5")
+            dir_agent_params = Path(dir_save, "agent_params.pt")
 
             evaluate(env, agent, des, cfg, dir_env_data, dir_agent_data)
             draw_plot(dir_env_data, dir_agent_data, dir_save)